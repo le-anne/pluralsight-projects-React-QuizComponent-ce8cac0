00313c715eff9eeb748b2ee20f638ffe5b0f5440
Leanne <76232496+le-anne@users.noreply.github.com>
2022-06-28 09:59:16 -0700

    starting over

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import "./App.css";
+
+let quizData = require("./quiz_data.json");
 
 class App extends Component {
   render() {
-    return <div />;
+    return <Quiz />;
   }
 }
+export default Quiz;
