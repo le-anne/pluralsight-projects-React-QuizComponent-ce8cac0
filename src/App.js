fc036aebd7fb093cf871da2a90e99e8346865d67
Leanne <76232496+le-anne@users.noreply.github.com>
2022-06-28 10:23:21 -0700

    pluralsight issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-import Quiz from "./Quiz.js";
 
 class App extends Component {
   render() {
-    return <Quiz />;
+    return <div />;
   }
 }
-
-export default App;
