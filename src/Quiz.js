395e9715e3446d1643af3ac0b2594208e3a3b2a5
Leanne <76232496+le-anne@users.noreply.github.com>
2022-06-27 21:16:27 -0700

    checking work on pluralsight

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
-import "./App.css";
-import Quiz from "./Quiz.js";
 
-class App extends Component {
+let quizData = require("./quiz_data.json");
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
   render() {
-    return <Quiz />;
+    return (
+      <div>
+        <div className="QuizQuestion">
+          {quizData.quiz_questions[0].instruction_text}
+        </div>
+      </div>
+    );
   }
 }
+
+export default Quiz;
