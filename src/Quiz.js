780e99f23e280f4d745f1cf3e4a78fbf181c8390
Leanne <76232496+le-anne@users.noreply.github.com>
2022-06-27 21:13:57 -0700

    started project

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,42 +1,9 @@
 import React, { Component } from "react";
-import QuizQuestion from "./QuizQuestion";
-import QuizEnd from "./QuizEnd";
+import "./App.css";
+import Quiz from "./Quiz.js";
 
-let quizData = require("./quiz_data.json");
-
-class Quiz extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { quiz_position: 1 };
-  }
-  handleResetClick() {
-    this.setState({ quiz_position: 1 });
-  }
-  showNextQuestion() {
-    this.setState((state) => {
-      return { quiz_position: state.quiz_position + 1 };
-    });
-  }
+class App extends Component {
   render() {
-    const isQuizEnd =
-      this.state.quiz_position - 1 === quizData.quiz_questions.length;
-    return (
-      <div>
-        <div>
-          {isQuizEnd ? (
-            <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
-          ) : (
-            <QuizQuestion
-              quiz_question={
-                quizData.quiz_questions[this.state.quiz_position - 1]
-              }
-              showNextQuestionHandler={this.showNextQuestion.bind(this)}
-            />
-          )}
-        </div>
-      </div>
-    );
+    return <Quiz />;
   }
 }
-
-export default Quiz;
